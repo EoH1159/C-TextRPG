b2d1196e0e5e1eb5deb91c5234a40ada289149c6
EoH1159 <zien33195@gmail.com>
2025-10-05 15:39:59 +0900

    Add files via upload

---
+++ Aincrad6/step6.cs
@@ -0,0 +1,106 @@
+﻿// step6 공통
+
+//1.
+int[] score = new int[5];
+    score[0] = 100;
+    score[2] = 80;
+    if (score[0] ==100)
+{
+    Console.WriteLine("만점입니다!");
+}
+Console.WriteLine(score[0] + score[2]);
+
+//2.
+string[] items = {"철검", "가죽 갑옷", "Hp포션"};
+string item = items[2];
+int count = items.Length;
+Console.WriteLine(item);
+Console.WriteLine(count);
+for (int i = 0; i < items.Length ; i++)
+{
+    Console.WriteLine(items[i]);
+}
+
+//3.
+string[] monsternames = new string[3];
+monsternames[0] = "슬라임";
+monsternames[1] = "골렘";
+monsternames[2] = "킬러 머신";
+for (int i = 0;i < monsternames.Length ; i++)
+{
+    Console.WriteLine($"{i}번 몬스터 : {monsternames[i]}");
+}
+Console.WriteLine("====================");
+foreach (string monstername in monsternames)
+{ Console.WriteLine(monstername); }
+
+//4.
+int[] data = { 5, 12, 3, 8, 10 };
+for (int i = 0; i < data.Length ; i++)
+{
+    if(data[i] > 10)
+    {
+        Console.WriteLine(data[i]);
+    }
+}
+Console.WriteLine("====================");
+for (int i = 0; i < data.Length; i++)
+{
+    if (data[i] % 2 == 0)
+    {
+        Console.WriteLine(data[i]);
+    }
+}
+Console.WriteLine("====================");
+int sum = 0;
+for (int i = 0; i < data.Length; i++)
+{
+  sum += data[i];
+}
+Console.WriteLine(sum);
+Console.WriteLine("====================");
+foreach (int datas in data)
+{
+    if (datas > 5)
+    {
+        Console.WriteLine(datas);
+    }
+}
+Console.WriteLine("====================");
+
+//5.
+List<string> inventory = new List<string>();
+inventory.Add("검");
+inventory.Add("방패");
+inventory.Add("포션");
+int count2 = inventory.Count;
+Console.WriteLine(count2);
+string item2 = inventory[0];
+Console.WriteLine(item2);
+inventory[1] = "강철 방패";
+inventory.Remove("포션");
+inventory.RemoveAt(0);
+int count3 = inventory.Count;
+Console.WriteLine(count3);
+bool hasShield = inventory.Contains("강철 방패");
+Console.WriteLine(hasShield);
+bool hasSword = inventory.Contains("검");
+Console.WriteLine(hasSword);
+inventory.Clear();
+int count4 = inventory.Count;
+Console.WriteLine(count4);
+static void PrintInventory(List<string>inv)
+{
+    foreach (string item2 in inv)
+        { Console.WriteLine(item2); }
+}
+
+//기동 키
+void main()
+{
+    inventory.Add("검");
+    inventory.Add("방패");
+    inventory.Add("포션");
+    PrintInventory(inventory);
+}
+main();
---
+++ Aincrad7/step7.cs
@@ -0,0 +1,127 @@
+﻿// Step7 basic
+using System.Collections.Generic;
+
+void main()
+{
+    Player player1 = new Player("전사", 100);
+    player1.Attack();
+    player1.TakeDamage(30);
+    Monster slime = new Monster("슬라임", 1);
+    slime.Displayinfo();
+    Monster goblin = new Monster("고블린", 3);
+    goblin.hp = 10;
+    goblin.Displayinfo();
+    Item potion = new Item("hp포션", 50);
+    Item sword = new Item("철검", 150);
+    sword.price = 200;
+    potion.Displayinfo2();
+    sword.Displayinfo2();
+    Skill Majinken = new Skill("마신검", 50, 20);
+    Skill heal = new Skill("힐", -30, 15);
+    Majinken.damage = 60;
+    Majinken.Activate();
+    heal.Activate();
+    Shop shop = new Shop();
+    shop.AddItem(new Item("hp포션", 50));
+    shop.AddItem(new Item("mp포션", 70));
+    shop.AddItem(new Item("철검", 150));
+    shop.items[0].price = 60;
+    shop.dispplayItems();
+}
+main();
+
+//1.
+class Player
+{
+    public string name;
+    public int hp;
+    public Player(string name, int hp)
+    {
+        this.name = name;
+        this.hp = hp;
+    } //생성자추가, 초기화
+    public void Attack()
+    {
+        Console.WriteLine($"{name}이(가) 공격합니다!");
+    }
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        Console.WriteLine($"{name}이(가) {damage} 데미지를 입음 남은 hp: {hp}");
+    }
+}
+
+//2.
+class Monster
+{
+    public string name;
+    public int level;
+    public int hp;
+    public Monster(string name, int level)
+    {
+        this.name = name;
+        this.level = level;
+        this.hp = level * 50;
+    }
+    public void Displayinfo()
+    {
+        Console.WriteLine($"이름: {name} \n레벨: {level} \nHP: {hp}");
+    }
+}
+
+//3.
+class Item
+{
+    public string name;
+    public int price;
+    public Item(string name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+    public void Displayinfo2()
+    {
+        Console.WriteLine($"아이템: {name} \n가격: {price}G");
+    }
+}
+
+//4.
+class Skill
+{
+    public string name;
+    public int damage;
+    public int mpCost;
+    public Skill(string name, int damage, int mpCost)
+    {
+        this.name = name;
+        this.damage = damage;
+        this.mpCost = mpCost;
+    }
+    public void Activate()
+    {
+        Console.WriteLine($"{name} 발동 데미지: {damage} mp {mpCost}소모");
+    }
+}
+
+//5.
+class Shop
+{
+    public List<Item> items;
+    public Shop() 
+    { items = new List<Item>(); }
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+        Console.WriteLine($"상점에 {item.name}추가됨");
+    }
+    public void dispplayItems()
+    {
+        Console.WriteLine("--- 상점 목록 ---");
+        foreach (Item item in items)
+        {
+            item.Displayinfo2();
+        }
+    }
+}
+
+//팁: 컨트롤 r을 두 번 누르면 한꺼번에 함수라던가 이름을 바꿀 수 있다.
